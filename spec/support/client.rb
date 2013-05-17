de1e858d431492cc1a965a511ecd8f7c2a70b5ba
Steven Wagner <leftathome@gmail.com>
2013-05-17 10:26:13 -0700

    Ensuring that :node_name is set.

--- spec/support/client.rb
+++ spec/support/client.rb
@@ -1,4 +1,4 @@
-log_level	:info
-log_location	STDOUT
-chef_server_url	"https://api.opscode.com/organizations/chef-rundeck"
-node_name	"travis"
+log_level       :info
+log_location    STDOUT
+chef_server_url "https://api.opscode.com/organizations/chef-rundeck"
+node_name       "travis"
