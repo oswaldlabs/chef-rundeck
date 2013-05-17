92cf6ae73dd84ef0393dbf45a24ff9f2a5f3078c
Steven Wagner <leftathome@gmail.com>
2013-05-17 10:19:57 -0700

    Paths now relative to root of repo.

--- spec/support/setup-test.sh
+++ spec/support/setup-test.sh
@@ -2,5 +2,5 @@
 
 # um ... so, obviously, don't run this on a Chef-managed workstation.
 mkdir -p /etc/chef/
-cp client.rb /etc/chef/
-cp travis.pem /etc/chef/client.pem
+cp spec/support/client.rb /etc/chef/
+cp spec/support/travis.pem /etc/chef/client.pem
