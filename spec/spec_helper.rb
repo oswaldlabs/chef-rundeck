b68b62b3db90e746aaee5c63e7e7ee68f9e630b9
Brian Scott <brainscott@gmail.com>
2013-05-17 20:10:00 -0700

    Fixed failed spec in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,19 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'chef-rundeck'
+require 'sinatra'
 require 'rspec'
 require 'rspec/autorun'
 require 'rack/test'
 
-# setup test environment
-# set :environment, :test
-# set :run, false
-# set :raise_errors, true
-# set :logging, false
+
+set :environment, :test
+set :run, false
+set :raise_errors, true
+set :logging, false
 
   def app
     ChefRundeck.new
-    ChefRundeck.config_file("#{ENV['TRAVIS_BUILD_DIR']}/spec/support/client.rb")
   end
 
 RSpec.configure do |config|
