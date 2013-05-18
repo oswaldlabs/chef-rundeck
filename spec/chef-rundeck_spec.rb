9a5fc66deb0513889de53a372b73031c99d5a6b8
Brian Scott <Brian.L.Scott@Disney.com>
2013-05-17 20:16:32 -0700

    Spec test

--- spec/chef-rundeck_spec.rb
+++ spec/chef-rundeck_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe 'ChefRundeck' do
-
+  ChefRundeck.config_file = "#{ENV['TRAVIS_BUILD_DIR']}/spec/support/client.rb"
   it 'should return false' do
-  	ChefRundeck.new
-  	ChefRundeck.config_file = "#{ENV['TRAVIS_BUILD_DIR']}/spec/support/client.rb"
-
     get '/' 
     last_response.should be_ok
   end
