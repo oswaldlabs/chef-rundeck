5940e4fbeadc2fa486ce84d518fb538e7592b5c5
Brian Scott <Brian.L.Scott@Disney.com>
2013-05-17 20:10:00 -0700

    Fixed failed spec in spec_helper

--- spec/chef-rundeck_spec.rb
+++ spec/chef-rundeck_spec.rb
@@ -1,7 +1,13 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
-describe "ChefRundeck" do
-  it "should respond to GET" do
-    get '/'
+describe 'ChefRundeck' do
+
+  it 'should return false' do
+  	ChefRundeck.new
+  	ChefRundeck.config_file = "#{ENV['TRAVIS_BUILD_DIR']}/spec/support/client.rb"
+
+    get '/' 
+    last_response.should be_ok
   end
+
 end
