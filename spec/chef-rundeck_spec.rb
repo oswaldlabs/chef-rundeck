1e01640fa5c5d3a84d3bdce36577364e9cb57f28
Joseph Martin <jsmartin@softlayer.com>
2013-05-24 12:51:46 -0500

    Fix the test to actually use the config_file we set and expect it to not be_ok

--- spec/chef-rundeck_spec.rb
+++ spec/chef-rundeck_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'ChefRundeck' do
   ChefRundeck.config_file = "#{ENV['TRAVIS_BUILD_DIR']}/spec/support/client.rb"
+  ChefRundeck.configure
   it 'should return false' do
     get '/' 
-    last_response.should be_ok
+    last_response.should_not be_ok
   end
-
 end
