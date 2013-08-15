3e7e7632a1e39b95124221ba0fd9d7e69373dc5f
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2013-08-15 23:40:01 +0000

    Set ChefRundeck.username in test

--- spec/chef-rundeck_spec.rb
+++ spec/chef-rundeck_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'ChefRundeck' do
   it 'should return false' do
     ChefRundeck.config_file = "#{ENV['TRAVIS_BUILD_DIR']}/spec/support/client.rb"
+    ChefRundeck.username = ENV['USER']
     ChefRundeck.configure
 
     get '/' 
