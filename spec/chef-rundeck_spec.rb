e1a24b232522dfda24d52b1701152fd3958c0ca5
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2013-08-15 23:46:03 +0000

    Set web_ui_url in test

--- spec/chef-rundeck_spec.rb
+++ spec/chef-rundeck_spec.rb
@@ -4,6 +4,7 @@ describe 'ChefRundeck' do
   it 'should return false' do
     ChefRundeck.config_file = "#{ENV['TRAVIS_BUILD_DIR']}/spec/support/client.rb"
     ChefRundeck.username = ENV['USER']
+    ChefRundeck.web_ui_url = 'https://manage.opscode.com'
     ChefRundeck.configure
 
     get '/' 
